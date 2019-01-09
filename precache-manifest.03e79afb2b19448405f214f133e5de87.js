d5b2858a85b952acdb5955ed6cd4d14f7f4cbf51
Clay Tsay <claytsay@users.noreply.github.com>
2019-01-08 16:42:02 -0800

    Updates

--- precache-manifest.03e79afb2b19448405f214f133e5de87.js
+++ precache-manifest.03e79afb2b19448405f214f133e5de87.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/class_planner/static/js/runtime~main.c0cdcba9.js"
   },
   {
-    "revision": "93952d034e11c5cf4cdb",
-    "url": "/class_planner/static/js/main.93952d03.chunk.js"
+    "revision": "25cd77c857bd2a08aaec",
+    "url": "/class_planner/static/js/main.25cd77c8.chunk.js"
   },
   {
     "revision": "f03e8ae863144a61c620",
     "url": "/class_planner/static/js/1.f03e8ae8.chunk.js"
   },
   {
-    "revision": "93952d034e11c5cf4cdb",
+    "revision": "25cd77c857bd2a08aaec",
     "url": "/class_planner/static/css/main.abc03532.chunk.css"
   },
   {
-    "revision": "fccf6e440226d11ff86ac7a243835c2c",
+    "revision": "bc226044023ce8d316bcc3da44ee4ac8",
     "url": "/class_planner/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/class_planner/precache-manifest.2400ada81756580b73260bacf333986d.js"
+  "/class_planner/precache-manifest.03e79afb2b19448405f214f133e5de87.js"
 );
 
 workbox.clientsClaim();
